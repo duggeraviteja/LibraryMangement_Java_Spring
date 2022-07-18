a855a4c1b07fde61e7f34b2e89a89087c376d18a
duggeraviteja <72975888+duggeraviteja@users.noreply.github.com>
2022-07-19 00:00:19 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/controller/BookController.java
@@ -0,0 +1,106 @@
+package com.controller;
+
+import com.model.Book;
+import com.model.User;
+import com.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Random;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @RequestMapping("/get")
+    public List<Book> getBooks() {
+        return bookService.getBooks();
+    }
+
+    @RequestMapping("/get/{id}")
+    public List<Book> getBookById(@PathVariable("id") int bookId) {
+        return bookService.getBookById(bookId);
+    }
+
+    @PostMapping("/create")
+    public Book createBook(@RequestBody Book book) {
+        Random random = new Random();
+        int number = random.nextInt(999999);
+        book.setStock(1);
+        book.setBookId(number);
+        bookService.createBook(book);
+        return book;
+    }
+
+    @RequestMapping("/getMostReadBook")
+    public List<Book> mostReadBookId() {
+        int bookId = bookService.getMostReadBookId();
+        List<Book> bookList = bookService.getBookById(bookId);
+        return bookList;
+    }
+
+    @RequestMapping("/searchBookName/{bookName}")
+    public List<Book> SearchBookName(@PathVariable("bookName") String bookName) {
+        return bookService.getBookByName(bookName);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public Book deleteBook(@PathVariable("id") int bookId){
+        return bookService.deleteBook(bookId);
+    }
+
+    @DeleteMapping("deleteuser/{id}")
+    public User deleteUserById(@PathVariable("id") int id){
+        return bookService.deleteUserById(id);
+    }
+
+    @RequestMapping("/getUser")
+    public List<User> getUsers() {
+        return bookService.getUsers();
+    }
+
+    @RequestMapping("/getUser/{key}")
+    public List<User> getUsers(@PathVariable("key") String key) {
+        return bookService.searchUsers(key);
+    }
+
+    @RequestMapping("/totalUsers")
+    public int totalUsers(){
+       List<User> userList =  bookService.getUsers();
+        return  userList.size();
+    }
+    @RequestMapping("/totalBooks")
+    public int totalBooks(){
+        List<Book> userList =  bookService.getBooks();
+        return  userList.size();
+    }
+
+    @RequestMapping("/lastaddedBooks")
+    public int lastAddedBooks(){
+        return bookService.lastAddedBooks();
+    }
+
+    @RequestMapping("/categoryWiseBooks")
+    public  List<Book> categoryWiseBooks() {
+        return  bookService.categoryWiseBooks();
+    }
+
+    @PostMapping("/createUser")
+    public User createUser(@RequestBody User user) {
+
+        Random random = new Random();
+        int number = random.nextInt(999999);
+        user.setUserId(number);
+
+        if(user.getImage()==null){
+            user.setImage("https://picsum.photos/225");
+        }
+
+        return bookService.createUser(user);
+    }
+
+}
---
+++ FrontEnd/js/index.js
@@ -0,0 +1,183 @@
+
+
+const newbook = async (e) => {
+  // let bookId = document.getElementById('bookId').value;
+  
+  let bookName = document.getElementById('bookName').value;
+  let author = document.getElementById('author').value;
+  let category = document.getElementById('category').value;
+  let shelfNumber = document.getElementById('shelfNumber').value;
+
+  if ( !bookName) {
+    document.getElementById('bookName').style = 'border: 2px solid red !important';
+    return launch_toast("fail", "Book Name Cannot be null");
+  }
+  if(  !author) {
+    document.getElementById('author').style = 'border: 2px solid red !important';
+    return launch_toast("fail", "Author  Cannot be null");
+  } 
+  if( !category) {
+    document.getElementById('category').style = 'border: 2px solid red !important';
+    return launch_toast("fail", "category Cannot be null");
+  }
+  if( !shelfNumber) {
+    document.getElementById('shelfNumber').style = 'border: 2px solid red !important';
+    return launch_toast("fail", "shelfNumber Cannot be null");
+
+  }
+
+
+  let data = {
+    bookName: bookName,
+    author: author,
+    category: category,
+    shelfNumber: shelfNumber
+  };
+
+
+  try {
+    await fetch('http://localhost:8080/LibraryManagement/create', {
+      method: 'POST',
+      headers: {
+        'Access-Control-Allow-Origin': "*",
+        'mode': 'no-cors',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(response => response.json())
+      .then(data => {
+        if(data) {
+          booksData();
+        }
+         launch_toast("success", "Successfully Added new Book with Book ID " + data.bookId);
+      });
+
+  } catch (error) {
+    launch_toast("fail", error)
+  }
+
+  
+  document.getElementById('bookName').value = "";
+  document.getElementById('author').value = "";
+  document.getElementById('category').value = "";
+  document.getElementById('shelfNumber').value = "";
+
+}
+
+
+
+const launch_toast = (status, msg) => {
+
+  var x = document.getElementById("toast")
+  var img = document.getElementById("img");
+
+  if (status === "success") {
+    x.style.backgroundColor = "green";
+    img.innerHTML = ` <i class="fa fa-check-circle fa-2x mb-4" aria-hidden="true"></i>`;
+    img.style.backgroundColor = "green";
+    x.appendChild(img);
+  }
+  else if (status === "fail") {
+    x.style.backgroundColor = "red";
+    img.style.backgroundColor = "red";
+    img.innerHTML = ` <i class="fa fa-times fa-2x mb-4" aria-hidden="true"></i>`;
+    x.appendChild(img);
+  } else {
+    x.style.backgroundColor = "yellow";
+    img.style.backgroundColor = "yellow";
+    img.innerHTML = `<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> `
+    x.appendChild(img);
+  }
+  var desc = document.getElementById("desc");
+  desc.innerHTML = msg;
+  x.appendChild(desc);
+  x.className = "show";
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, 5000);
+}
+
+
+
+
+
+
+
+
+//  dashboard
+
+
+
+const totalBooks = async ()=>{
+  await fetch("http://localhost:8080/LibraryManagement/totalBooks")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("totalBooks").innerHTML = data;
+  });
+  totalMembers();
+  totalFineCollected();
+  totalBooksIssuedToday();
+}
+
+const totalMembers =async ()=>{
+  await fetch("http://localhost:8080/LibraryManagement/totalUsers")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("totalUsers").innerHTML = data;
+  });
+
+  
+}
+
+const totalBooksIssuedToday =async ()=>{
+  await fetch("http://localhost:8080/LibraryManagement/bookissuedtoday")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("bookissuedtoday").innerHTML = data;
+  });
+  
+}
+
+const totalFineCollected = async()=>{
+  await fetch("http://localhost:8080/LibraryManagement/totalFine")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("totalfine").innerHTML = "₹ "+ data+".0";
+  });
+  
+}
+
+
+
+const booksData = async () => {
+  let books;
+  await fetch("http://localhost:8080/LibraryManagement/get")
+    .then(response => response.json())
+    .then(data => {
+      books = data;
+    });
+  if (books) {
+    tdBody.innerHTML = "";
+    cardPrinting2(books);
+  }
+}
+
+
+let cardPrinting2 = (books) => {
+  books.map((element,index) => {
+  
+let content = `
+              <tr  scope="row"> 
+              <td > ${index+1}   </td>
+              <td > ${element.bookId}   </td>
+              <td > ${element.bookName}   </td>
+              <td > ${element.author}   </td>
+              <td > ${element.category}   </td>
+              <td > ${element.shelfNumber}   </td>
+              <td >   <i class="fa fa-trash" aria-hidden="true" onclick="deleteBook(${element.id})"></i>
+              </tr>
+
+`
+    tdBody.innerHTML += content;
+
+  });
+}
